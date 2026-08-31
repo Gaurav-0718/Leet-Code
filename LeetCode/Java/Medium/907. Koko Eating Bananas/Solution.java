eb0aafef6e575184f3aa69ede4864e269aaa44ee
Vadrevu Sai Siddharth Gaurav <25b21ds012@adityauniversity.in>
2026-08-31 23:36:17 +0530

    Add Medium Java Solution for 907. Koko Eating Bananas from LeetCode

---
+++ LeetCode/Java/Medium/907. Koko Eating Bananas/Solution.java
@@ -0,0 +1,34 @@
+// Binary Search on Answer, 875. Koko Eating Bananas
+
+class Solution {
+    public static boolean canEat(int[] piles, int hoursHave, int k) {
+        // Compute the hours needed to eat all piles at k bananas / per hour speed
+        long hoursNeeded = 0;
+        for (int pile: piles) {
+            hoursNeeded += (int)Math.ceil(1f * pile / k);
+        }
+        return hoursNeeded <= hoursHave;
+    }
+    public static int getMax(int[] piles) {
+        int max = piles[0];
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max) max = piles[i];
+        }
+        return max;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        // Binary Search on Answer
+        int low = 1;
+        int high = getMax(piles); // max(piles)
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (canEat(piles, h, mid)) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
