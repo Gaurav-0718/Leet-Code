af5c1687b23938dbeb501909900a5d6b7bec11c8
Vadrevu Sai Siddharth Gaurav <25b21ds012@adityauniversity.in>
2026-08-31 11:21:28 +0530

    Add Easy Python Solution for 409. Longest Palindrome from LeetCode

---
+++ LeetCode/Python/Easy/409. Longest Palindrome/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        a = {}
+        ans=0
+        flag=False
+        for i in s:
+            
+            a[i]=a.get(i,0)+1
+        for i in a:
+            ans+=(a[i]//2)*2
+            if not flag:
+                if a[i]%2==1:
+                    ans+=1
+                    flag=True
+
+        return ans
