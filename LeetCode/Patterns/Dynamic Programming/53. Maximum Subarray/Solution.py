1ef8d898361df1d13e71972f105c77b4927619cd
Vadrevu Sai Siddharth Gaurav <25b21ds012@adityauniversity.in>
2026-09-01 11:28:05 +0530

    Add Medium Python Solution for 53. Maximum Subarray from LeetCode

--- LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.py
+++ LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
         left = 0
-        ans = nums[0]
         curr = 0
+        ans = nums[0]
 
-        for i in range(n):
+        for i in range(len(nums)):
             curr += nums[i]
 
-            if curr < 0:
-                ans = max(ans, curr)
-
-            while left <= i and curr < 0:
-                curr -= nums[left]
-                left += 1
-
             ans = max(ans, curr)
+
+            if curr < 0:
+                curr = 0
+                left = i + 1
 
         return ans
